d79d4f9b4413a63b8ae9c3306d4f33acffdcf8b4
ritagord <gordiichuk.ma@phystech.edu>
2021-09-14 21:24:42 +0300

    try_2.sh

---
+++ try_2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd ~
+
+echo "time:" | tee info.txt 
+
+date | tee -a  info.txt
+
+echo "username:" $USER | tee -a info.txt
+
+echo "os:" $OSTYPE | tee -a info.txt
+
+
+echo "home directory:" | tee -a  info.txt
+pwd | tee -a  info.txt
+
+echo "files(recursive):" | tee -a  info.txt
+ls -R | wc -l | tee -a info.txt
+
+echo "folders(non recursive):" | tee -a info.txt
+ls -l  |grep "^d"| wc -l | tee -a  info.txt
+
+
+
+
